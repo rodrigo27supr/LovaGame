caf8fd3c6e484968e80fa68f2b538c96b45805fc
Rodrigo Cuéllar Londoño <184222321+rodrigo27supr@users.noreply.github.com>
2025-12-26 22:48:56 +0100

    Update Notificador.java

--- src/main/java/com/rodrigo/lovagame/Notificador.java
+++ src/main/java/com/rodrigo/lovagame/Notificador.java
@@ -9,7 +9,7 @@ public class Notificador {
 
     // GitHub Secrets
     private static final String TOKEN = System.getenv("TELEGRAM_TOKEN");
-    private static final String CHAT_ID = System.getenv("@lovagame_rodrigo");
+    private static final String CHAT_ID = System.getenv("TELEGRAM_CHAT_ID");
 
     // FOTO + TEXTO + BOTN
     public void enviarFoto(String urlImagen, String textoCaption, String urlOferta) {
